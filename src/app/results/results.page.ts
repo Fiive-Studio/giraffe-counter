0489d9f11d82aa10aee1cececfa30ef4dce19088
Pablo Diaz <pablodiazvalencia@hotmail.com>
2020-06-25 18:33:20 -0500

    - Add process to calculate values - Change theme

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayersService } from '../services/players.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
   selector: 'app-results',
@@ -9,20 +11,87 @@ import { Router } from '@angular/router';
 })
 export class ResultsPage implements OnInit {
 
+  private inputs: any[];
+  private defaultName = 'player';
+
   constructor(private playersService: PlayersService
+    , public alertController: AlertController
+    , private utils: UtilsService
     , private router: Router) { }
 
   ngOnInit() {
-    console.log(this.playersService.getCount().length);
-    if (this.playersService.getCount().length > 0) {
-      this.playersService.setResult([0, 1]);
-      this.playersService.setResult([0, 2]);
-      this.playersService.setResult([3, 5]);
-    }
   }
 
   redirectToPlayers() {
     this.router.navigateByUrl('/players');
   }
 
+  async showAlertResults() {
+
+    this.createInputs();
+    const alert = await this.alertController.create({
+      header: 'Giraffe',
+      message: 'Agregar resultados',
+      inputs: this.inputs,
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary'
+      }, {
+        text: 'Aceptar',
+        handler: (data) => {
+          console.log(data);
+          return this.addResults(data);
+        }
+      }],
+      cssClass: 'alert-style'
+    });
+
+    await alert.present();
+  }
+
+  addResults(data: any) {
+    if (this.validateData(data)) {
+
+      let newResults = new Array<number>();
+      for (let x = 0; x < this.playersService.getCount().length; x++) {
+        newResults.push(data[this.defaultName + x.toString()]);
+      }
+
+      this.playersService.addResults(newResults);
+      return true;
+    }
+
+    return false;
+  }
+
+  validateData(data: any): boolean {
+    for (let x = 0; x < this.playersService.getCount().length; x++) {
+
+      if (this.utils.isNullOrEmpty(data[this.defaultName + x.toString()])) {
+        this.utils.showAlert("Error", "Debe digitar los resultados de todos los jugadores y deben ser numéricos");
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  createInputs() {
+    if (this.inputs === undefined) {
+
+      this.inputs = new Array(this.playersService.getCount().length);
+      let players = this.playersService.getPlayers();
+
+      for (let x = 0; x < players.length; x++) {
+        this.inputs.push(
+          {
+            name: this.defaultName + x.toString(),
+            type: 'number',
+            placeholder: players[x]
+          }
+        );
+      }
+    }
+  }
 }
--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -7,37 +7,58 @@ import { UtilsService } from './utils.service';
 export class PlayersService {
 
   private players: string[];
+  private playersAbbr: string[];
   private results: number[][];
+  private resultsTotal: number[][];
   private count: number[];
 
   constructor(private utils: UtilsService) {
     this.results = new Array<number[]>();
+    this.resultsTotal = new Array<number[]>();
     this.count = new Array<number>(0);
-   }
+  }
 
   setPlayersCount(count: number) {
     this.players = new Array<string>(count);
     this.count = new Array<number>(count);
+    this.playersAbbr = new Array<string>(count);
   }
 
   getCount() { return this.count; }
   getPlayers() { return this.players; }
+  getPlayersAbbr() { return this.playersAbbr; }
+  getResults() { return this.results; }
+  getResultsTotal() { return this.resultsTotal; }
 
-  setResult(values: number[]) {
+  addResults(values: number[]) {
     this.results.push(new Array<number>(this.players.length));
     this.results[this.results.length - 1] = values;
+
+    this.addResultsTotal(values);
   }
 
-  getResults() {
-    return this.results;
+  addResultsTotal(values: number[]) {
+
+    if (this.results.length > 0) {
+      this.resultsTotal.push(new Array<number>(this.players.length));
+
+      if (this.results.length > 1) {
+        for (let x = 0; x < values.length; x++) {
+          values[x] = parseInt(values[x].toString()) + parseInt(this.results[this.results.length - 2][x].toString());
+        }
+      }
+
+      this.resultsTotal[this.results.length - 1] = values;
+    }
   }
 
   setPlayer(name: string, pos: number) {
     this.players[pos] = name;
+    this.setPlayerAbbr(pos);
   }
 
-  getPlayer(pos: number): string {
-    return this.players[pos];
+  setPlayerAbbr(pos: number) {
+    this.playersAbbr[pos] = this.players[pos][0];
   }
 
   validateNames(): boolean {
